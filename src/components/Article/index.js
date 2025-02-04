b2e0ed2affe6c677304c8580ebf7d047b119d6b8
RafaelPrisco <rafaelprisco23@gmail.com>
2025-02-04 14:30:13 -0300

    Creating the structure of components.

---
+++ src/components/Article/index.js
@@ -0,0 +1,14 @@
+import Post from '../Post';
+
+function Article(){
+    return(
+        
+        <>
+        <h1>Article</h1>
+        <Post></Post>
+        
+        </>
+    );
+}
+
+export default Article;
---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+
+
+const Header = () => {
+
+
+    
+    let contador = 8;
+    
+    
+    return (
+        <h1>Header.js {contador}</h1>
+    );
+}
+
+
+export default Header;
